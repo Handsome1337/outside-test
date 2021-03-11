5d2eadbbfca1a11c7e930a79d76046ea474f5dba
Ilya Volkov <volkov-ilyha@ya.ru>
2021-03-11 23:16:09 +0300

    Добавляет компонент App и кнопку «Налоговый вычет»

---
+++ src/components/app/app.js
@@ -0,0 +1,11 @@
+import './app.css';
+
+function App() {
+  return (
+    <main className="outside-digital-app">
+      <button className="tax-deduction-button">Налоговый вычет</button>
+    </main>
+  );
+}
+
+export default App;
